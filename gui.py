d33c5bf8510f9464a92a6c4d4b892073575fca1c
salmi younes <shinigamir913@gmail.com>
2024-09-07 21:10:50 +0100

    gui update

--- gui.py
+++ gui.py
@@ -1,106 +1,179 @@
-
+#! /usr/bin/env python3
 from chess import Chess, Color
 import pygame
+from enum import Enum
 from pygame.locals import MOUSEBUTTONUP
 import sys
 
-pygame.init()
+class AllPieces(Enum):
+    empty = 0
+    bP = 1 
+    bR = 2 
+    bN = 3 
+    bB = 4 
+    bQ = 5 
+    bK = 6 
+    wP = 7 
+    wR = 8 
+    wN = 9 
+    wB = 10 
+    wQ = 11 
+    wK = 12
 
-WIDTH, HEIGHT = 600, 600
-SQ_SIZE = WIDTH // 8
-LIGHT_BROWN = (181, 136, 99)
-DARK_BROWN = (101, 67, 33)
+class ChessGui:
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Chess")
+    pygame.init()
 
-chess_game = Chess()
-chess_game.board_init()
-chess_game.bit_board_init()
+    WIDTH, HEIGHT = 600, 600
+    SQ_SIZE = WIDTH // 8
+    LIGHT_BROWN = (181, 136, 99)
+    DARK_BROWN = (101, 67, 33)
+    HIGHLIGHT_COLOR = (255, 255, 0)
 
-board = [
-    ['bR', 'bN', 'bB', 'bK', 'bQ', 'bB', 'bN', 'bR'],
-    ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP'],
-    ['--', '--', '--', '--', '--', '--', '--', '--'],
-    ['--', '--', '--', '--', '--', '--', '--', '--'],
-    ['--', '--', '--', '--', '--', '--', '--', '--'],
-    ['--', '--', '--', '--', '--', '--', '--', '--'],
-    ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
-    ['wR', 'wN', 'wB', 'wK', 'wQ', 'wB', 'wN', 'wR']
-]
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Chess")
 
-def draw_board():
-    for row in range(8):
-        for col in range(8):
-            color = LIGHT_BROWN if (row + col) % 2 == 0 else DARK_BROWN
-            pygame.draw.rect(screen, color, pygame.Rect(col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+    chess_game = Chess()
+    chess_game.board_init()
+    chess_game.bit_board_init()
 
-def update_board(x_src, y_src, x_dst, y_dst, piece, color):
-    moves = chess_game.gen_moves(color)
-    for move in moves:
-        if move.piece == 0:
-            break
-        if move.src == 63 - (x_src * 8 + y_src) and move.dst == 63 - (x_dst * 8 + y_dst):
-            board[x_src][y_src], board[x_dst][y_dst] = '--', piece
-            chess_game.do_move(move)
-            return True
-    return False
+    board = [[AllPieces.empty.value for _ in range(8)] for _ in range(8)]
 
-def load_pieces():
-    return {piece: pygame.image.load(f'img/{piece}.png') for piece in ['bP', 'bR', 'bN', 'bB', 'bQ', 'bK', 'wP', 'wR', 'wN', 'wB', 'wQ', 'wK']}
+    pieces = {}
+    sounds = {}
 
-def draw_pieces(board, pieces):
-    for row in range(8):
-        for col in range(8):
-            piece = board[row][col]
-            if piece != '--':
-                p_img = pieces[piece].convert_alpha()
-                p_img_rect = p_img.get_rect()
-                p_img_rect.centerx, p_img_rect.centery = col * SQ_SIZE + SQ_SIZE // 2, row * SQ_SIZE + SQ_SIZE // 2
-                screen.blit(p_img, p_img_rect)
+    @staticmethod
+    def preload_assets():
+        ChessGui.pieces = {num: pygame.image.load(f'img/{piece}.png') for num, piece in enumerate(
+            ['bP', 'bR', 'bN', 'bB', 'bQ', 'bK', 'wP', 'wR', 'wN', 'wB', 'wQ', 'wK'], start=1)}
+        ChessGui.sounds['move'] = pygame.mixer.Sound('sound/assets_sounds_move.wav')
+        ChessGui.sounds['capture'] = pygame.mixer.Sound('sound/assets_sounds_capture.wav')
 
-def switch_player(color):
-    return Color.BLACK.value if color == Color.WHITE.value else Color.WHITE.value
+    @staticmethod
+    def draw_board():
+        for row in range(8):
+            for col in range(8):
+                color = ChessGui.LIGHT_BROWN if (row + col) % 2 == 0 else ChessGui.DARK_BROWN
+                pygame.draw.rect(ChessGui.screen, color, pygame.Rect(col * ChessGui.SQ_SIZE, row * ChessGui.SQ_SIZE, ChessGui.SQ_SIZE, ChessGui.SQ_SIZE))
 
-def play_sound(is_capture):
-    sound_file = 'sound/assets_sounds_capture.wav' if is_capture else 'sound/assets_sounds_move.wav'
-    sound = pygame.mixer.Sound(sound_file)
-    pygame.mixer.Sound.play(sound)
+    @staticmethod
+    def update_board(x_src, y_src, x_dst, y_dst, piece, color):
+        moves = ChessGui.chess_game.gen_moves(color)
+        for move in moves:
+            if move.piece == 0:
+                break
+            if move.src == 63 - (x_src * 8 + y_src) and move.dst == 63 - (x_dst * 8 + y_dst):
+                ChessGui.board[x_src][y_src], ChessGui.board[x_dst][y_dst] = AllPieces.empty.value, piece
+                ChessGui.chess_game.do_move(move)
+                return True
+        return False
 
-def main():
-    pieces = load_pieces()
-    running = True
-    player = Color.WHITE.value
-    src = False
-    x_src, y_src = None, None
+    @staticmethod
+    def draw_pieces(board):
+        for row in range(8):
+            for col in range(8):
+                piece = board[row][col]
+                if piece != AllPieces.empty.value:
+                    p_img = ChessGui.pieces[piece].convert_alpha()
+                    p_img_rect = p_img.get_rect()
+                    p_img_rect.centerx, p_img_rect.centery = col * ChessGui.SQ_SIZE + ChessGui.SQ_SIZE // 2, row * ChessGui.SQ_SIZE + ChessGui.SQ_SIZE // 2
+                    ChessGui.screen.blit(p_img, p_img_rect)
 
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == MOUSEBUTTONUP:
-                y, x = pygame.mouse.get_pos()
-                x, y = x // SQ_SIZE, y // SQ_SIZE
-                
-                if not src:
-                    if board[x][y] != '--':  
-                        x_src, y_src = x, y
-                        src = True
-                else:
-                    x_dst, y_dst = x, y
-                    is_capture = board[x_dst][y_dst] != '--'
-                    if update_board(x_src, y_src, x_dst, y_dst, board[x_src][y_src], player):
-                        play_sound(is_capture)
-                        player = switch_player(player)
-                        chess_game.print_board()
-                    src = False
+    @staticmethod
+    def switch_player(color):
+        return Color.BLACK.value if color == Color.WHITE.value else Color.WHITE.value
 
-        draw_board()
-        draw_pieces(board, pieces)
-        pygame.display.flip()
+    @staticmethod
+    def play_sound(is_capture):
+        sound = ChessGui.sounds['capture'] if is_capture else ChessGui.sounds['move']
+        pygame.mixer.Sound.play(sound)
 
-    pygame.quit()
-    sys.exit()
+    @staticmethod
+    def make_board():
+        for i in range(8):
+            for j in range(8):
+                pos = i * 8 + j
+                if ChessGui.chess_game.board.WhitePawns & (1 << pos):
+                    ChessGui.board[i][j] = AllPieces.wP.value
+                elif ChessGui.chess_game.board.BlackPawns & (1 << pos):
+                    ChessGui.board[i][j] = AllPieces.bP.value
+                elif ChessGui.chess_game.board.WhiteRooks & (1 << pos):
+                    ChessGui.board[i][j] = AllPieces.wR.value
+                elif ChessGui.chess_game.board.BlackRooks & (1 << pos):
+                    ChessGui.board[i][j] = AllPieces.bR.value
+                elif ChessGui.chess_game.board.WhiteKnights & (1 << pos):
+                    ChessGui.board[i][j] = AllPieces.wN.value
+                elif ChessGui.chess_game.board.BlackKnights & (1 << pos):
+                    ChessGui.board[i][j] = AllPieces.bN.value
+                elif ChessGui.chess_game.board.WhiteBishops & (1 << pos):
+                    ChessGui.board[i][j] = AllPieces.wB.value
+                elif ChessGui.chess_game.board.BlackBishops & (1 << pos):
+                    ChessGui.board[i][j] = AllPieces.bB.value
+                elif ChessGui.chess_game.board.WhiteQueens & (1 << pos):
+                    ChessGui.board[i][j] = AllPieces.wQ.value
+                elif ChessGui.chess_game.board.BlackQueens & (1 << pos):
+                    ChessGui.board[i][j] = AllPieces.bQ.value
+                elif ChessGui.chess_game.board.WhiteKing & (1 << pos):
+                    ChessGui.board[i][j] = AllPieces.wK.value
+                elif ChessGui.chess_game.board.BlackKing & (1 << pos):
+                    ChessGui.board[i][j] = AllPieces.bK.value
+        ChessGui.board[:] = ChessGui.board[::-1]
+
+    @staticmethod
+    def draw_moves(x_src, y_src, color):
+        moves = ChessGui.chess_game.gen_moves(color)
+        valid_moves = [moves[i].dst for i in range(256) if moves[i].src == 63 - (x_src * 8 + y_src)]
+        for move in valid_moves:
+            row, col = divmod(63 - move, 8)
+            if row in range(64) and col in range(64): 
+                if ChessGui.board[row][col] == AllPieces.empty.value:
+                    pygame.draw.circle(ChessGui.screen, (220,20,60), (col * ChessGui.SQ_SIZE + ChessGui.SQ_SIZE // 2, row * ChessGui.SQ_SIZE + ChessGui.SQ_SIZE // 2), 10)
+                else :
+                    pygame.draw.rect(ChessGui.screen,ChessGui.HIGHLIGHT_COLOR, pygame.Rect(col * ChessGui.SQ_SIZE, row * ChessGui.SQ_SIZE, ChessGui.SQ_SIZE, ChessGui.SQ_SIZE))
+
+        pygame.draw.rect(ChessGui.screen,ChessGui.HIGHLIGHT_COLOR, pygame.Rect(y_src * ChessGui.SQ_SIZE, x_src * ChessGui.SQ_SIZE, ChessGui.SQ_SIZE, ChessGui.SQ_SIZE))
+
+
+    @staticmethod
+    def main():
+        ChessGui.preload_assets()
+        ChessGui.make_board()
+        running = True
+        player = Color.WHITE.value
+        src_selected = False
+        x_src, y_src = None, None
+        draw = False
+
+        while running:
+            if not draw:
+                ChessGui.draw_board()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.MOUSEBUTTONUP:
+                    y, x = pygame.mouse.get_pos()
+                    x, y = x // ChessGui.SQ_SIZE, y // ChessGui.SQ_SIZE
+                    if not src_selected:
+                        if ChessGui.board[x][y] != AllPieces.empty.value:
+                            x_src, y_src = x, y
+                            src_selected = True
+                            draw = True
+                            ChessGui.draw_moves(x_src, y_src, player)
+                    else:
+                        x_dst, y_dst = x, y
+                        is_capture = ChessGui.board[x_dst][y_dst] != AllPieces.empty.value
+                        if ChessGui.update_board(x_src, y_src, x_dst, y_dst, ChessGui.board[x_src][y_src], player):
+                            ChessGui.play_sound(is_capture)
+                            player = ChessGui.switch_player(player)
+                            ChessGui.chess_game.print_board()
+                        src_selected = False
+                        draw = False
+            ChessGui.draw_pieces(ChessGui.board)
+
+            pygame.display.flip()
+
+        pygame.quit()
+        sys.exit()
 
 if __name__ == "__main__":
-    main()
+    ChessGui.main()
